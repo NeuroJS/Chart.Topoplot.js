3f524543c95ab5794189928eb921fa9b7ec96bd7
andrewheusser <andrew.heusser@gmail.com>
2016-04-14 11:10:53 -0400

    saving..

---
+++ index.js
@@ -0,0 +1,24 @@
+var topogrid = require('topogrid')
+
+// x coordinates of the data
+var pos_x = [1,2,3,4,5,6,7,8];
+
+// y coordinates of the data
+var pos_y = [1,2,3,4,5,6,7,8];
+
+// the data values
+var data = getRandomInts(0, 10, 8)
+
+function getRandomInts(min, max, num) {
+  var data = []
+    for(i=0; i<num+1; i++){
+      data.push(Math.floor(Math.random() * (max - min)) + min)
+    }
+  return data
+};
+
+// the parameters for the grid [x,y,z] where x is the min of the grid, y is the
+// max of the grid and z is the number of points
+var grid_params = [0,10,11];
+
+zi = topogrid.create(pos_x,pos_y,data,grid_params);
